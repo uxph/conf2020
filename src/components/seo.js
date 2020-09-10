8717a268b9c0b1e77a615c93f18d8de8e9f43f50
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2020-09-10 19:11:18 +0800

    implemented chatwoot

--- src/components/seo.js
+++ src/components/seo.js
@@ -43,29 +43,19 @@ function SEO({ description, lang, meta, title }) {
         </script>
         <script>
           {`
-"use strict";
-!function() {
-  var t = window.driftt = window.drift = window.driftt || [];
-  if (!t.init) {
-    if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
-    t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
-    t.factory = function(e) {
-      return function() {
-        var n = Array.prototype.slice.call(arguments);
-        return n.unshift(e), t.push(n), t;
-      };
-    }, t.methods.forEach(function(e) {
-      t[e] = t.factory(e);
-    }), t.load = function(t) {
-      var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
-      o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
-      var i = document.getElementsByTagName("script")[0];
-      i.parentNode.insertBefore(o, i);
-    };
+(function(d,t) {
+  var BASE_URL = "https://app.chatwoot.com";
+  var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+  g.src= BASE_URL + "/packs/js/sdk.js";
+  s.parentNode.insertBefore(g,s);
+  g.onload=function(){
+    window.chatwootSDK.run({
+      websiteToken: 'q6wSADRgSBkoJk5c572RJ2KW',
+      baseUrl: BASE_URL
+    })
   }
-}();
-drift.SNIPPET_VERSION = '0.3.1';
-drift.load('m6viw39wy699');
+})(document,"script");
+
 `}
         </script>
       </Helmet>
