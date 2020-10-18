3e3416e2620d97b76f1282117abbe1aa447a7d4f
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2020-10-18 21:27:34 +0800

    Updated schedule and program

--- src/components/schedule/programme.js
+++ src/components/schedule/programme.js
@@ -72,96 +72,103 @@ const DaySegment = ({ segment, setSegmentName, setWorkshopId, toggle }) => {
       index - 1 >= 0
         ? schedule[segment][index].time === schedule[segment][index - 1].time
         : false;
-    return (
-      <div
-        className={`padding-y-32`}
-        style={{
-          borderTop: isSameTime ? "none" : "thin solid #f3f3f3",
-        }}
-        key={event.id}
-      >
-        {/* {!isSpecialSegment && (
+
+    if (!isSpecialSegment && event.description[0] === "Coming Soon!") {
+      return null;
+    } else {
+      return (
+        <div
+          className={`padding-y-32`}
+          style={{
+            borderTop: isSameTime ? "none" : "thin solid #f3f3f3",
+          }}
+          key={event.id}
+        >
+          {/* {!isSpecialSegment && (
           <h6 className="gray mx-4 margin-bottom-32">
             <strong>{event.time}</strong>
           </h6>
         )} */}
-        <a
-          href="/"
-          className={`${
-            isSpecialSegment ? "" : "segment-item"
-          } bg-white d-block`}
-          style={{
-            textDecoration: "none",
-            cursor: isSpecialSegment ? "default" : "pointer",
-          }}
-          onClick={(e) => {
-            e.preventDefault();
-            if (!isSpecialSegment) {
-              setSegmentName(segment);
-              setWorkshopId(event.id);
-              toggle();
-              // console.log(segment + " " + event.id);
-            }
-          }}
-        >
-          <Row key={index} className={`mx-3 segment-container`}>
-            {isSameTime ? (
-              <Col md={2} sm={12}></Col>
-            ) : (
-              <Col md={2} sm={12}>
-                <h6 className="gray">
-                  <strong>{event.time}</strong>
-                </h6>
-              </Col>
-            )}
-
-            <Col md={4} sm={12}>
-              <h5
-                className={`margin-bottom-12 ${isSpecialSegment ? "mb-0" : ""}`}
-                style={{
-                  lineHeight: "1.5rem",
-                  fontSize: "1rem",
-                }}
-              >
-                {event.title}
-              </h5>
-              {!isSpecialSegment && (
-                <div className="mb-1">
-                  <Chip
-                    variant="outlined"
-                    label={event.type}
-                    className="margin-bottom-8"
-                    style={{
-                      // color: "#ffffff",
-                      // backgroundColor: "#e8006f",
-                      borderColor: "#e8006f",
-                      color: "#e8006f",
-                      fontFamily: "Karla",
-                      fontSize: "0.7rem",
-                    }}
-                  />
-                  {currWorkshops}
-                </div>
+          <a
+            href="/"
+            className={`${
+              isSpecialSegment ? "" : "segment-item"
+            } bg-white d-block`}
+            style={{
+              textDecoration: "none",
+              cursor: isSpecialSegment ? "default" : "pointer",
+            }}
+            onClick={(e) => {
+              e.preventDefault();
+              if (!isSpecialSegment) {
+                setSegmentName(segment);
+                setWorkshopId(event.id);
+                toggle();
+                // console.log(segment + " " + event.id);
+              }
+            }}
+          >
+            <Row key={index} className={`mx-3 segment-container`}>
+              {isSameTime ? (
+                <Col md={2} sm={12}></Col>
+              ) : (
+                <Col md={2} sm={12}>
+                  <h6 className="gray">
+                    <strong>{event.time}</strong>
+                  </h6>
+                </Col>
               )}
-            </Col>
-            <Col md={6} sm={12} className="pl-4 speaker-list">
-              <div>{facilitators}</div>
-              {!isSpecialSegment && (
-                <Button
-                  variant="outline"
-                  className="d-none event-read-more"
+
+              <Col md={4} sm={12}>
+                <h5
+                  className={`margin-bottom-12 ${
+                    isSpecialSegment ? "mb-0" : ""
+                  }`}
                   style={{
-                    padding: "0.7rem 0.7rem",
+                    lineHeight: "1.5rem",
+                    fontSize: "1rem",
                   }}
                 >
-                  Read more
-                </Button>
-              )}
-            </Col>
-          </Row>
-        </a>
-      </div>
-    );
+                  {event.title}
+                </h5>
+                {!isSpecialSegment && (
+                  <div className="mb-1">
+                    <Chip
+                      variant="outlined"
+                      label={event.type}
+                      className="margin-bottom-8"
+                      style={{
+                        // color: "#ffffff",
+                        // backgroundColor: "#e8006f",
+                        borderColor: "#e8006f",
+                        color: "#e8006f",
+                        fontFamily: "Karla",
+                        fontSize: "0.7rem",
+                      }}
+                    />
+                    {currWorkshops}
+                  </div>
+                )}
+              </Col>
+              <Col md={6} sm={12} className="pl-4 speaker-list">
+                <div>{facilitators}</div>
+                {!isSpecialSegment && (
+                  <Button
+                    variant="outline"
+                    className="d-none event-read-more"
+                    style={{
+                      padding: "0.7rem 0.7rem",
+                    }}
+                  >
+                    Read more
+                  </Button>
+                )}
+              </Col>
+            </Row>
+          </a>
+        </div>
+      );
+    }
   });
 
   return <>{currSegment}</>;
