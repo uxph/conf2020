5ece0dfd74592c60812e7d7673e849ca0b6101f6
mjarenyap <mjaren.yap@gmail.com>
2020-11-08 13:55:44 +0800

    Refined the certificate generator flow

--- src/pages/certificate.js
+++ src/pages/certificate.js
@@ -7,6 +7,7 @@ import _ from "lodash";
 
 import attendees from "../data/attendees.json";
 import info from "../data/info.json";
+import $ from "jquery";
 
 const Certificate = () => {
   const [generate, setGenerate] = useState(false);
@@ -18,30 +19,58 @@ const Certificate = () => {
   const [certName, setCertName] = useState("");
 
   const verify = () => {
-    if (!_.isNil(attendees[orderNo])) {
+    if (!_.isNil(attendees[orderNo.toLowerCase()])) {
       setIsValidOrderNo(true);
-      setCertName(attendees[orderNo].name);
+      setCertName(attendees[orderNo.toLowerCase()].name);
       setValidMessage(null);
     } else {
       setIsValidOrderNo(false);
       setValidMessage(
         <p className="red text-center">
-          Order No. <strong>{orderNo}</strong> is invalid.
+          Order No. <strong>{orderNo.toUpperCase()}</strong> is invalid.
         </p>
       );
     }
   };
+
+  // useEffect to auto scroll to the certificate preview
+  useEffect(() => {
+    if (isValidOrderNo) {
+      $("html, body").animate(
+        {
+          scrollTop: $("#generate-certificate").offset().top - 50,
+        },
+        400
+      );
+
+      console.log(document.getElementById("generate-certificate").offsetTop);
+    }
+  }, [isValidOrderNo, setIsValidOrderNo]);
 
   // useEffect to generate the certificate
   useEffect(() => {
     if (generate) {
       const doc = new jsPDF("landscape"); // todo compress PDF specs
 
-      // doc.addImage("/images/logos/uxph_conf_logo.png", "PNG", 15, 40, 180, 180);
+      doc.addImage(
+        "/images/certificates/cert-template.png",
+        "PNG",
+        0,
+        0,
+        180,
+        180
+      );
       // TODO add proper font
       // TODO add the certificate visuals to the export
-      doc.text(certName, 10, 10); // todo position the name properly
-      doc.save(`UXPH Conf 2020 Certificate of Attendance - ${certName}.pdf`);
+      doc.text(certName.toUpperCase(), 10, 10); // todo position the name properly
+      doc.save(
+        `UXPH Conf 2020 Certificate of Attendance - ${certName
+          .split(" ")
+          .map(
+            (x) => x[0].toUpperCase() + x.substring(1, x.length).toLowerCase()
+          )
+          .join(" ")}.pdf`
+      );
     }
 
     setGenerate(false);
@@ -70,23 +99,40 @@ const Certificate = () => {
           }}
         ></div>
         <div
-          className="margin-top-256 margin-bottom-48 mx-auto"
+          className={`${
+            isValidOrderNo ? "margin-top-96" : "margin-top-256"
+          } margin-bottom-48 mx-auto`}
           style={{
             position: "relative",
             zIndex: "500",
             width: "700px",
             boxSizing: "border-box",
-            // top: "50%",
-            // left: "50%",
-            // transform: "translate(-50%, -50%)",
           }}
           id="confirmation-content"
         >
+          {/* Shows up if the Order No. is valid */}
           {isValidOrderNo ? (
-            <div id="generate-certificate">
+            <div id="generate-certificate" className="margin-top-192">
               <center>
                 {/* This is where they'll download the certificate */}
                 {/* Add a preview certificate template with an editable name field */}
+                <small
+                  className="text-uppercase text-white"
+                  style={{
+                    letterSpacing: "1px",
+                  }}
+                >
+                  Your name on the certificate
+                </small>
+                <Input
+                  type="text"
+                  className="my-2 text-center font-weight-bold text-uppercase text-white"
+                  placeholder="Your name on the certificate"
+                  value={certName}
+                  onChange={(event) => setCertName(event.target.value)}
+                  id="certificate-name-input"
+                />
+                <br />
                 <Button onClick={() => setGenerate(true)}>
                   Claim your certificate
                 </Button>
@@ -108,8 +154,8 @@ const Certificate = () => {
                   <Input
                     type="text"
                     id="order-no-field"
-                    className="w-100 my-2"
-                    placeholder="Order No."
+                    className="w-100 my-2 text-uppercase"
+                    placeholder="Ticket No."
                     onChange={(event) => setOrderNo(event.target.value)}
                     onKeyPress={(event) => {
                       if (event.key === "Enter") {
@@ -135,42 +181,43 @@ const Certificate = () => {
                 </Col>
               </Row>
               {validMessage}
-              <div className="text-center margin-top-64">
-                <p className="text-white margin-bottom-16">
-                  Join the community and follow us for updates!
-                </p>
-              </div>
-              <div className="d-flex justify-content-center margin-top-16">
-                <a href={info.facebook_page} target="blank">
-                  <img
-                    src={"/images/icons/facebook-square.svg"}
-                    alt="UXPH Facebook Page"
-                    className="margin-right-24"
-                  />
-                </a>
-                <a href={info.instagram} target="blank">
-                  <img
-                    src={"/images/icons/ig-square.svg"}
-                    alt="UXPH Instagram Page"
-                    className="margin-right-24"
-                  />
-                </a>
-                <a href={info.twitter} target="blank">
-                  <img
-                    src={"/images/icons/twitter.svg"}
-                    alt="UXPH Twitter Page"
-                    className="margin-right-24"
-                  />
-                </a>
-                <a href={info.linkedin} target="blank">
-                  <img
-                    src={"/images/icons/linkedin.svg"}
-                    alt="UXPH LinkedIn Page"
-                  />
-                </a>
-              </div>
             </div>
           )}
+
+          <div className="text-center margin-top-64">
+            <p className="text-white margin-bottom-16">
+              Join the community and follow us for updates!
+            </p>
+          </div>
+          <div className="d-flex justify-content-center margin-top-16">
+            <a href={info.facebook_page} target="blank">
+              <img
+                src={"/images/icons/facebook-square.svg"}
+                alt="UXPH Facebook Page"
+                className="margin-right-24"
+              />
+            </a>
+            <a href={info.instagram} target="blank">
+              <img
+                src={"/images/icons/ig-square.svg"}
+                alt="UXPH Instagram Page"
+                className="margin-right-24"
+              />
+            </a>
+            <a href={info.twitter} target="blank">
+              <img
+                src={"/images/icons/twitter.svg"}
+                alt="UXPH Twitter Page"
+                className="margin-right-24"
+              />
+            </a>
+            <a href={info.linkedin} target="blank">
+              <img
+                src={"/images/icons/linkedin.svg"}
+                alt="UXPH LinkedIn Page"
+              />
+            </a>
+          </div>
         </div>
       </header>
     </>
